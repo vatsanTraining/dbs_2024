04764ad9c041917c6141836aaabad3dded493f27
srivatsan <vatsan_k@hotmail.com>
2024-07-22 16:45:58 +0530

    IO Example Added

---
+++ io_examples/src/com/example/demo/impl/CrudRepositoryImpl.java
@@ -0,0 +1,82 @@
+package com.example.demo.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.ifaces.CrudRepository;
+import com.example.demo.model.Product;
+import java.io.*;
+public class CrudRepositoryImpl implements CrudRepository {
+	
+	
+	
+	private List<Product> productList;
+	PrintWriter writer;
+
+	
+
+	public CrudRepositoryImpl() throws IOException {
+		super();
+		this.productList = new ArrayList<>();
+		
+		 
+	}
+
+	@Override
+	public boolean add(Product object) {
+
+		 boolean response =false;
+
+		
+		 try {
+			writer =new PrintWriter(new FileWriter(new File("Product.txt"),true));
+			writer.println(object);
+            response=true;
+
+		 } catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+		
+			 writer.close();
+		
+		}
+
+		 
+		 
+		
+		
+		return response;
+	}
+
+	@Override
+	public List<Product> findAll() {
+		
+	
+		BufferedReader reader=null;
+		try {
+			 reader = new BufferedReader(new FileReader(new File("Product.txt")));
+			
+			String line =null;
+			
+			while((line=reader.readLine())!=null){
+				
+				System.out.println(line);
+			}
+		} catch ( IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+		
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		return this.productList;
+	}
+
+}
---
+++ io_examples/src/com/example/demo/model/Product.java
@@ -0,0 +1,72 @@
+package com.example.demo.model;
+
+import com.example.demo.exceptions.RangeCheckException;
+
+public class Product {
+
+	
+	private int productId;
+	private String productName;
+	private double ratePerUnit;
+	
+	public Product() {
+		super();
+	}
+
+	public Product(int productId, String productName, double ratePerUnit) throws Exception {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		
+		if(ratePerUnit>0) {
+			
+		this.ratePerUnit = ratePerUnit;
+		
+		} else {
+			throw new RangeCheckException("Invalid Rate Per Unit - Should be Positive Number");
+		}
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getRatePerUnit() {
+		return ratePerUnit;
+	}
+
+	public void setRatePerUnit(double ratePerUnit)  throws Exception{
+		
+		
+		
+		if(ratePerUnit>0) {
+			
+		this.ratePerUnit = ratePerUnit;
+		
+		} else {
+			throw new RangeCheckException("Invalid Rate Per Unit - Should be Positive Number");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return this.productName + this.ratePerUnit;
+	}
+	
+	
+	
+	
+	
+}
