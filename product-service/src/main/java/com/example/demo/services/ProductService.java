0b69eb2cd07d7184512a6b02b57a3470660cb448
srivatsan <vatsan_k@hotmail.com>
2024-07-23 15:31:22 +0530

    Update

--- product-service/src/main/java/com/example/demo/services/ProductService.java
+++ product-service/src/main/java/com/example/demo/services/ProductService.java
@@ -3,11 +3,13 @@ package com.example.demo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.ifaces.CrudRepository;
 import com.example.demo.impl.CrudRepositoryImpl;
 import com.example.demo.model.Product;
 
+@Service
 public class ProductService {
 
 	
