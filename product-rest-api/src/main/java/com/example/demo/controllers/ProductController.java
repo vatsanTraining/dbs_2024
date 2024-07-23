7a304d564bb4fc30da8919822a5701375faa87e2
srivatsan <vatsan_k@hotmail.com>
2024-07-23 17:43:03 +0530

    End of Day Two

---
+++ product-rest-api/src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Product;
+import com.example.demo.services.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+	
+	ProductService  service;
+
+	// Constructor DI
+	public ProductController(ProductService service) {
+		super();
+		this.service = service;
+	}
+	
+	
+	@GetMapping
+	public List<Product> findAll(){
+		
+		return this.service.findAll();
+	}
+}
