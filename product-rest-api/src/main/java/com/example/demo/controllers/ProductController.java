2b964b059e3230d9c412968358a253be6b595f60
srivatsan <vatsan_k@hotmail.com>
2024-07-23 17:43:48 +0530

    End of Day Two

--- product-rest-api/src/main/java/com/example/demo/controllers/ProductController.java
+++ product-rest-api/src/main/java/com/example/demo/controllers/ProductController.java
@@ -14,7 +14,7 @@ import com.example.demo.services.ProductService;
 public class ProductController {
 
 	
-	ProductService  service;
+	private ProductService  service;
 
 	// Constructor DI
 	public ProductController(ProductService service) {
