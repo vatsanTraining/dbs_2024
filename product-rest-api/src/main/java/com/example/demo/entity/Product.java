8331d827255b57c9344bbcd8eb4638b2d07b0572
srivatsan <vatsan_k@hotmail.com>
2024-07-23 17:35:26 +0530

    REST API Added

---
+++ product-rest-api/src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,60 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_2024")
+public class Product {
+
+	
+	@Id
+	@Column(name = "product_id")
+	private int productId;
+	
+	
+	@Column(name = "product_name")
+	private String productName;
+	
+	@Column(name = "rate_per_unit")
+	private double ratePerUnit;
+
+	public Product() {
+		super();
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getRatePerUnit() {
+		return ratePerUnit;
+	}
+
+	public void setRatePerUnit(double ratePerUnit) {
+		this.ratePerUnit = ratePerUnit;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", ratePerUnit=" + ratePerUnit
+				+ "]";
+	}
+	
+	
+
+}
---
+++ product-rest-api/src/main/java/com/example/demo/services/ProductService.java
@@ -0,0 +1,64 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Product;
+import com.example.demo.ifaces.ProductRepository;
+
+@Service
+public class ProductService {
+
+	
+	private ProductRepository repo;
+
+	// ProductRepository Implementation will be Injected Here
+	// @Autowired is not required since there is only one constructor
+	// Adding a @autowired may show a warning
+	
+	
+	public ProductService(ProductRepository repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	public List<Product> findAll(){
+		
+		return this.repo.findAll();
+	}
+	
+	
+	public Product  save(Product obj) {
+		
+		return this.repo.save(obj);
+	}
+	
+	
+	public Product findById(int key) {
+	
+		return this.repo.findById(key).orElseThrow(
+				 () -> new RuntimeException("Product with Given Id Not Found"));
+	}
+	public Product update(Product obj) {
+		
+		return this.repo.save(obj);
+	}
+	
+	public boolean remove(int key) {
+		
+		boolean isDeleted =false;
+		if(this.repo.existsById(key)) {
+			
+			this.repo.deleteById(key);
+			
+			isDeleted = true;
+		}
+		
+		return isDeleted;
+		
+	}
+	
+	
+	
+}
