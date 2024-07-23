4c70a166b92e050a3d8f774f9c999d38fd5214d6
srivatsan <vatsan_k@hotmail.com>
2024-07-23 16:43:08 +0530

    Update

--- product-service-rdbms/src/main/java/com/example/demo/ProductServiceRdbmsApplication.java
+++ product-service-rdbms/src/main/java/com/example/demo/ProductServiceRdbmsApplication.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.services.ProductService;
 
 @SpringBootApplication
 public class ProductServiceRdbmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceRdbmsApplication.class, args);
+		 ConfigurableApplicationContext ctx=SpringApplication.run(ProductServiceRdbmsApplication.class, args);
+	
+	
+		 ctx.getBean(ProductService.class)
+		      .findAll()
+		       .forEach(System.out::println);
+		 
+		 
+		 
+		 ctx.close();
+	
 	}
 
 }
