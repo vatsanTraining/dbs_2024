d59fee99fbd29827f26fac51a19e02de0bf1db4a
srivatsan <vatsan_k@hotmail.com>
2024-07-24 15:08:58 +0530

    Design Pattern

---
+++ CreationalPatterns/src/com/training/singleton/PolicyQuote.java
@@ -0,0 +1,64 @@
+package com.training.singleton;
+
+public class PolicyQuote {
+
+	
+	private  String policyName;
+	private long tenure;
+	private double premium;
+	private String  paymentMode;
+	private static  PolicyQuote instance=null;
+	
+	public static PolicyQuote getInstance(){
+		
+		// First check
+		if(instance==null)
+		{
+		      synchronized (PolicyQuote.class) {
+		    	  
+		    	  //second check
+			   		if(instance == null){
+                   instance = new PolicyQuote();
+                   }
+                }
+		}
+		return instance;
+	}
+	
+	private  PolicyQuote() {
+		super();
+		System.out.println("Constructor Called");
+	}
+	
+	public String getPolicyName() {
+		return policyName;
+	}
+	public void setPolicyName(String policyName) {
+		this.policyName = policyName;
+	}
+	public long getTenure() {
+		return tenure;
+	}
+	public void setTenure(long tenure) {
+		this.tenure = tenure;
+	}
+	public double getPremium() {
+		return premium;
+	}
+	public void setPremium(double premium) {
+		this.premium = premium;
+	}
+	public String getPaymentMode() {
+		return paymentMode;
+	}
+	public void setPaymentMode(String paymentMode) {
+		this.paymentMode = paymentMode;
+	}
+	@Override
+	public String toString() {
+		return "PolicyQuote [policyName=" + policyName + ", tenure=" + tenure
+				+ ", premium=" + premium + ", paymentMode=" + paymentMode + "]";
+	}
+	
+	
+}
