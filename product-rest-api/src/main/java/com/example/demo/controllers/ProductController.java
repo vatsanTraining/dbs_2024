bf8495fa6bff2d1ace5ac46fa74b588272f4b790
srivatsan <vatsan_k@hotmail.com>
2024-07-24 10:18:54 +0530

    Post Method Added

--- product-rest-api/src/main/java/com/example/demo/controllers/ProductController.java
+++ product-rest-api/src/main/java/com/example/demo/controllers/ProductController.java
@@ -2,8 +2,13 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +39,13 @@ public class ProductController {
 	public Product findById(@PathVariable("id") int key){
 		
 		return this.service.findById(key);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Product> save(@RequestBody Product entity){
+		
+		     Product addedEntity= this.service.save(entity);
+		     
+		     return ResponseEntity.status(HttpStatus.CREATED).body(addedEntity);
 	}
 }
