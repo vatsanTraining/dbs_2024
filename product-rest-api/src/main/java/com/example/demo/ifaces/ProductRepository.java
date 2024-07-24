fc5f9d43dbf97ce719cc9c4dab6b5ac2e9cd59c3
srivatsan <vatsan_k@hotmail.com>
2024-07-24 10:44:18 +0530

    Update

--- product-rest-api/src/main/java/com/example/demo/ifaces/ProductRepository.java
+++ product-rest-api/src/main/java/com/example/demo/ifaces/ProductRepository.java
@@ -1,9 +1,28 @@
 package com.example.demo.ifaces;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entity.Product;
 
+import jakarta.websocket.server.PathParam;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+	//DSL findByPropName => if prop bookName it will findByBookName
+	
+	
+	public Product findByProductName(String name);
+	
+	//JPQL Java Persistence Query Language
+	// Uses concise query with className and property name instead of 
+	// tableName and column Name
+	
+	
+	@Query(value="from Product where ratePerUnit>:srchValue",nativeQuery = false)
+	public List<Product> getByRpuGrtThan(@Param("srchValue") double rpu);
+	
 }
