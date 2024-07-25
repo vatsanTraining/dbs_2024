4b8898c2b72510cc347b4456c4ad5f630c53190e
srivatsan <vatsan_k@hotmail.com>
2024-07-25 10:30:43 +0530

    RestTemplate Example Added

---
+++ product-rest-api-client/src/main/java/com/example/demo/ProductRestApiClientApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ProductRestApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRestApiClientApplication.class, args);
+	}
+
+	
+	@Bean
+	RestTemplate template() {
+		
+		return new RestTemplate();
+	}
+	
+	
+}
--- product-rest-api-client/src/test/java/com/example/demo/ProductRestApiClientApplicationTests.java
+++ product-rest-api-client/src/test/java/com/example/demo/ProductRestApiClientApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductRestApiApplicationTests {
+class ProductRestApiClientApplicationTests {
 
 	@Test
 	void contextLoads() {
