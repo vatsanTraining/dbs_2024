4fc7fadde79aea5bfee5a7746ba6faffd91e2891
srivatsan <vatsan_k@hotmail.com>
2024-07-25 17:01:23 +0530

    Project Completed

--- product-rest-api-client/src/main/java/com/example/demo/ProductRestApiClientApplication.java
+++ product-rest-api-client/src/main/java/com/example/demo/ProductRestApiClientApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +15,6 @@ public class ProductRestApiClientApplication {
 
 	
 	@Bean
-	@LoadBalanced
 	RestTemplate template() {
 		
 		return new RestTemplate();
